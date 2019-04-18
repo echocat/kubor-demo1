a8c81ec7998591b5fb103479bc57f231dc52e7fd
Gregor Noczinski <gregor@noczinski.eu>
2019-04-18 11:19:38 +0200

    Added more debugging output for development purposes

--- app.go
+++ app.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime"
+	"strconv"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -17,7 +20,7 @@ var (
 	branch   = "development"
 	revision = "latest"
 
-	readyAfter = flag.Duration("readyAfter", 15*time.Second, "Duration it takes after this service reports it is ready.")
+	readyAfter = flag.Duration("readyAfter", 0, "Duration it takes after this service reports it is ready.")
 	exitAfter  = flag.Duration("exitAfter", 0, "Duration it takes after this service dies with defined exit code."+
 		" This time starts after the service is ready. 0 == never exits.")
 	exitCode = flag.Int("exitCode", 1, "Code which will be used if this service exits after the defined duration.")
@@ -61,8 +64,10 @@ func runServer() {
 }
 
 func waitToBeReady() {
-	log.Printf("Waiting for %v to be ready...", *readyAfter)
-	time.Sleep(*readyAfter)
+	if *readyAfter > 0 {
+		log.Printf("Waiting for %v to be ready...", *readyAfter)
+		time.Sleep(*readyAfter)
+	}
 	ready.Store(true)
 }
 
@@ -83,21 +88,23 @@ func blockForEver() {
 }
 
 func handler(resp http.ResponseWriter, req *http.Request) {
-	if req.Method != "GET" {
-		http.Error(resp, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
 	switch req.URL.Path {
-	case "/":
-		handleRoot(resp, req)
 	case "/healthz":
 		handleHealth(resp, req)
 	default:
-		http.NotFound(resp, req)
+		handleEveryThingElse(resp, req)
 	}
 }
 
+func methodNotAllowed(resp http.ResponseWriter) {
+	http.Error(resp, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+}
+
 func handleHealth(resp http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		methodNotAllowed(resp)
+		return
+	}
 	r := ready.Load().(bool)
 	var v string
 	if r {
@@ -113,9 +120,52 @@ func handleHealth(resp http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func handleRoot(resp http.ResponseWriter, req *http.Request) {
-	resp.Header().Set("Content-Type", "text/plain")
-	if _, err := resp.Write([]byte("Hello world!")); err != nil {
+func handleEveryThingElse(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "application/json")
+	plainStatusCode := req.URL.Query().Get("statusCode")
+	if statusCode, err := strconv.Atoi(plainStatusCode); err == nil && statusCode >= 100 && statusCode < 1000 {
+		resp.WriteHeader(statusCode)
+	}
+	enc := json.NewEncoder(resp)
+	enc.SetIndent("", "  ")
+	body := responseBodyFor(req)
+	if err := enc.Encode(body); err != nil {
 		log.Printf("ERROR writing response to %v: %v", req.RemoteAddr, err)
 	}
+}
+
+func responseBodyFor(req *http.Request) (result responseBody) {
+	result.Runtime.Branch = branch
+	result.Runtime.Revision = revision
+	result.Runtime.Platform = runtime.GOOS + "-" + runtime.GOARCH
+
+	result.Request.Proto = req.Proto
+	result.Request.Host = req.Host
+	result.Request.Method = req.Method
+	result.Request.RequestURI = req.RequestURI
+	result.Request.Headers = req.Header
+	result.Request.Form = req.Form
+	result.Request.PostForm = req.PostForm
+	return
+}
+
+type responseBody struct {
+	Runtime runtimeBody `json:"runtime"`
+	Request requestBody `json:"request"`
+}
+
+type runtimeBody struct {
+	Branch   string `json:"branch"`
+	Revision string `json:"revision"`
+	Platform string `json:"platform"`
+}
+
+type requestBody struct {
+	Proto      string              `json:"proto"`
+	Host       string              `json:"host"`
+	Method     string              `json:"method"`
+	RequestURI string              `json:"requestURI"`
+	Headers    map[string][]string `json:"headers,omitempty"`
+	Form       map[string][]string `json:"form,omitempty"`
+	PostForm   map[string][]string `json:"postForm,omitempty"`
 }
